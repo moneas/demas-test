338857f23e042c36ffab85def6ff7471e9041a6a
moneas <demasww@gmail.com>
2023-10-19 12:30:10 +0800

    fix laravel-eloquent-relationship

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Task extends Model
 {
@@ -14,5 +15,10 @@ class Task extends Model
     public function user()
     {
         return $this->belongsTo(User::class, 'users_id');
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
     }
 }
